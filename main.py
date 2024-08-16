b7d2469f6c6c1bfc1de888c2e95412e48d020373
unknown <blandinedjoumsie@gmail.com>
2024-08-16 20:52:13 +0200

    created a file and added the conditions linked to the country

---
+++ main.py
@@ -0,0 +1,15 @@
+country = input("In which country do you want to drive(France, South Africa, Mexico, India)? ")
+user_Age = input("How old are you? ")
+
+if country == "France" :
+    print ("you chose France ")
+    
+elif country == "South Africa" :
+    print ("you chose South Africa ")
+    
+elif country == "South Mexico" :
+    print ("you chose Mexico ")
+    
+elif country == "South India" :
+    print ("you chose India ")
+    
