52212ea6569718f091becfb980d5b5eff5a4e148
unknown <blandinedjoumsie@gmail.com>
2024-08-17 15:07:42 +0200

    added conditions linked to the parameter age, then checked it

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 country = input("In which country do you want to drive(France, South Africa, Mexico, India)? ")
-user_Age = input("How old are you? ")
+Age = int(input("How old are you? "))
 
-if country == "France" :
-    print ("you chose France ")
+if country == "South Africa" :
+    if Age >= 17 :
+        print ("you can drive assisted by a 1 year old driver until you are a year older")
+    else:
+       print ("you can't drive") 
     
-elif country == "South Africa" :
-    print ("you chose South Africa ")
+elif country == "France" :
+    if Age >= 17 :
+        print ("you can drive with restrictions until you are 2 or 3 years older")
+    elif Age >= 15 :
+        print("you can drive under supervision")
+    else: 
+        print("you can't drive")
     
-elif country == "South Mexico" :
-    print ("you chose Mexico ")
+elif country == "Mexico" :
+    if Age >= 18 :
+        print ("you can drive")
+    elif Age >= 16 :
+        print("you can drive with parentale agreement")
+    elif Age == 15 :
+        print("you can drive under supervision")
+    else:
+        print("you can't drive")
     
-elif country == "South India" :
-    print ("you chose India ")
+elif country == "India" :
+    if Age >= 18 :
+        print ("you can drive")
+    else:
+        print("you can't drive")
+    
+else:
+    country != ("France, South Africa, Mexico, India")
+    print ("Data is not available for this country")
     
